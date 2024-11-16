23f72ec7b350ebd38ca887f817794acfc5c8ad01
eason9394 <60875629+eason9394@users.noreply.github.com>
2024-11-16 15:40:28 +0800

    linked list h update

--- LinkedList.h
+++ LinkedList.h
@@ -1,15 +1,25 @@
+#define WHOLEN 20
+#define WHATLEN 20
+#define WHENLEN 12
+#define WHERELEN 20
+
 typedef struct node
 {
-    int value;
+    //int phone;        i don't remeber rather we need phone number or not
+    char who[WHOLEN];
+    char what[WHATLEN];
+    char when[WHENLEN];
+    char where[WHERELEN];
     struct node * nextNodePtr;
 }Node;
 
 typedef struct list{
     Node * head;
+    Node * end;
     int count;
 }List;
 
 void newList(List**);
-void insert(List**, int);
+void insert(List**, const char*, const char*, const char*, const char*);
 void deleteNode(List**, int);
 
