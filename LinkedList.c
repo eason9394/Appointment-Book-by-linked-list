47c3638b6ecae880b1f1aad29922b10e40869a8b
eason9394 <60875629+eason9394@users.noreply.github.com>
2024-11-17 14:02:48 +0800

    updates of LinkedList.c , and Linkedlist.h

--- LinkedList.c
+++ LinkedList.c
@@ -1,9 +1,9 @@
 /*
-***************2024/11/16***************
+***************2024/11/17***************
+LinkedList.c Ver 2
     1.finish linked list function
-        -newList
-        -insert
-        -deleteNode
+        -add modify
+        -add search
                         writer : 松 
 ****************************************
 */
@@ -56,6 +56,7 @@ void insert(List **listptr, const char *who, const char *what, const char *when,
 }
 
 // Delete a node at a specific position in the list
+//position is 1-based
 void deleteNode(List **listptr, int position) {
     if ((*listptr)->count == 0) {
         printf("The list is empty. Nothing to delete.\n");
@@ -93,4 +94,118 @@ void deleteNode(List **listptr, int position) {
     (*listptr)->count--;
 
     printf("Node at position %d deleted successfully.\n", position);
+}
+
+void modify(List* list, int theCategoryOfChangeStaff, const char* updateString, int theCategoryOfFindStaff, const char* keystring){
+    if(list->count == 0){
+        printf("There's no appointment was written yet.\n");
+        return;
+    }
+
+    Node ** theChangingNodeptr = search(list, theCategoryOfFindStaff, keystring);
+
+    if(theChangingNodeptr == NULL){
+        printf("there's no such appointment record can be found in the file.\n");
+        return;
+    }
+
+    switch (theCategoryOfChangeStaff){
+    case who:                 //under the condition of changing who string
+        strcpy((*theChangingNodeptr)->who, updateString);
+        break;
+
+    case what:                 //under the condition of changing what string
+        strcpy((*theChangingNodeptr)->what, updateString);
+        break;
+
+    case when:                 //under the condition of changing when string
+        strcpy((*theChangingNodeptr)->when, updateString);
+        break;
+
+    case where:                 //under the condition of changing where string
+        strcpy((*theChangingNodeptr)->where, updateString);
+        break;
+
+    default:
+        printf("There is no such storied data type, please try again later.\n");
+        break;
+    }
+}
+
+Node** search(List* list, int theCategoryOfFindStaff, const char* keystring){
+    if(list->count == 0){
+        printf("There's no appointment was written yet.\n");
+        return;
+    }
+    
+    Node * searchingNode = list->head;
+
+    while (searchingNode != NULL){
+
+        int check;
+
+        switch (theCategoryOfFindStaff) {
+            case who:                 //under the condition of changing who string
+            if(strstr(searchingNode->who, keystring) != NULL){
+                printf("Who : %s\n", searchingNode->who);
+                printf("What : %s\n", searchingNode->what);
+                printf("When : %s\n", searchingNode->when);
+                printf("Where : %s\n", searchingNode->where);
+                printf("Is this the node you want to change?\n");
+                printf("For yes enter 1, as for no enter 0");
+                scanf("%d", &check);
+                if(check == 1)  return &searchingNode;
+            }
+            break;
+
+        case what:                 //under the condition of changing what string
+            if(strstr(searchingNode->what, keystring) != NULL){
+                printf("Who : %s\n", searchingNode->who);
+                printf("What : %s\n", searchingNode->what);
+                printf("When : %s\n", searchingNode->when);
+                printf("Where : %s\n", searchingNode->where);
+                printf("Is this the node you want to change?\n");
+                printf("For yes enter 1, as for no enter 0");
+                scanf("%d", &check);
+                if(check == 1)  return &searchingNode;
+            }
+            break;
+
+        case when:                 //under the condition of changing when string
+            if(strstr(searchingNode->when, keystring) != NULL){
+                printf("Who : %s\n", searchingNode->who);
+                printf("What : %s\n", searchingNode->what);
+                printf("When : %s\n", searchingNode->when);
+                printf("Where : %s\n", searchingNode->where);
+                printf("Is this the node you want to change?\n");
+                printf("For yes enter 1, as for no enter 0");
+                scanf("%d", &check);
+                if(check == 1)  return &searchingNode;
+            }
+            break;
+
+        case where:                 //under the condition of changing where string
+            if(strstr(searchingNode->where, keystring) != NULL){
+                printf("Who : %s\n", searchingNode->who);
+                printf("What : %s\n", searchingNode->what);
+                printf("When : %s\n", searchingNode->when);
+                printf("Where : %s\n", searchingNode->where);
+                printf("Is this the node you want to change?\n");
+                printf("For yes enter 1, as for no enter 0");
+                scanf("%d", &check);
+                if(check == 1)  return &searchingNode;
+            }
+            break;
+
+        default:
+            printf("There is no such storied data type, please try again later.\n");
+            break;
+        }
+
+        searchingNode = searchingNode->nextNodePtr;
+    }
+    if(searchingNode == NULL){
+        printf("There's no such appointment record in the file.\n");
+        return NULL;
+    }
 }
--- LinkedList.h
+++ LinkedList.h
@@ -3,6 +3,13 @@
 #define WHENLEN 12
 #define WHERELEN 20
 
+enum CategoryOfStoringData{
+  who = 1,
+  what = 2,
+  when = 3,
+  where = 4  
+};
+
 typedef struct node
 {
     //int phone;        i don't remeber rather we need phone number or not
@@ -22,4 +29,5 @@ typedef struct list{
 void newList(List**);
 void insert(List**, const char*, const char*, const char*, const char*);
 void deleteNode(List**, int);
-
+void modify(List*, int, const char*, int, const char*);
+Node** search(List*, int, const char*);
